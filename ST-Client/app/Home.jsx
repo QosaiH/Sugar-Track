6ebb4b2516f2c8426eea3160a615911126b9e790
soha ghanayem <gnaim.sa@gmail.com>
2025-04-27 12:04:01 +0300

    שלום instead of היי

--- ST-Client/app/Home.jsx
+++ ST-Client/app/Home.jsx
@@ -128,7 +128,8 @@ const styles = StyleSheet.create({
   },
   background: {
     flex: 1,
-    width:"100%",
+    width:'100%',
+    height:'100%',
     justifyContent: "space-around",
     alignItems: "center",
     paddingVertical: 25,
